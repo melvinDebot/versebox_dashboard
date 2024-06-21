74af0e1c47da2088113fc37cc9011a6ea6e2fd4c
Melvin Debot <melvin@exceenis.com>
2024-06-21 11:19:31 +0200

    feat: add function copy

--- src/components/Tables/TableUser.jsx
+++ src/components/Tables/TableUser.jsx
@@ -5,6 +5,7 @@ import { db } from "../../../firebase";
 import { ref, remove } from "firebase/database";
 
 const TableUser = ({ list }) => {
+  const [copiedUuid, setCopiedUuid] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState("");
   const itemsPerPage = 10;
@@ -29,6 +30,17 @@ const TableUser = ({ list }) => {
     if (window.confirm("Voulez-vous vraiment supprimer cet élément ?")) {
       handleDelete(path);
     }
+  };
+
+  const handleCopy = (uuidUser) => {
+    const copyText = document.getElementById(`npm-install-${uuidUser}`);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        setCopiedUuid(uuidUser);
+        setTimeout(() => setCopiedUuid(null), 2000); // Reset after 2 seconds
+      })
+      .catch((err) => console.error("Failed to copy!", err));
   };
 
   // // Filtrer les éléments en fonction du terme de recherche
@@ -100,13 +112,55 @@ const TableUser = ({ list }) => {
                   </span>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                  <input
-                    disabled
-                    type="checkbox"
-                    id="hs-basic-usage"
-                    checked={item.user.isCertified || false}
-                    className="relative w-[3.25rem] h-7 p-px bg-gray-100 border-transparent text-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-blue-600 disabled:opacity-50 disabled:pointer-events-none checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600 before:inline-block before:size-6 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-neutral-400 dark:checked:before:bg-blue-200"
-                  />
+                  <div className="grid grid-cols-8 gap-2 w-full max-w-[23rem]">
+                    <label className="sr-only">Label</label>
+                    <input
+                      id={`npm-install-${item.user.uuidUser}`}
+                      type="text"
+                      className="truncate col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      value={item.user.uuidUser}
+                      disabled
+                    />
+                    <button
+                      onClick={() => handleCopy(item.user.uuidUser)}
+                      data-copy-to-clipboard-target={item.user.uuidUser}
+                      className={`col-span-2 text-white ${copiedUuid !== item.user.uuidUser ? "bg-blue-700" : "bg-green-700"} font-medium rounded-lg text-sm w-full sm:w-auto py-2.5 text-center items-center inline-flex justify-center`}
+                    >
+                      {copiedUuid !== item.user.uuidUser ? (
+                        <span id="default-message">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z"
+                            />
+                          </svg>
+                        </span>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m4.5 12.75 6 6 9-13.5"
+                          />
+                        </svg>
+                      )}
+                    </button>
+                  </div>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <button
