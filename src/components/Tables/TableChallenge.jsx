517ba0ac89aa3e3c8cd9b3b7e28130b136b7eb36
Melvin Debot <melvin@exceenis.com>
2024-06-30 09:31:12 +0200

    feat: add button

--- src/components/Tables/TableChallenge.jsx
+++ src/components/Tables/TableChallenge.jsx
@@ -36,25 +36,32 @@ const TableChallenge = ({ list }) => {
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <div className="flex items-center space-x-3.5">
                     <button
-                      className="hover:text-primary"
                       onClick={() =>
                         navigation(
                           `/tables-challenges/${Object.keys(list)[key]}`,
                         )
                       }
+                      type="button"
+                      className="flex gap-0.5 focus:outline-none text-white bg-sky-400 hover:bg-sky-500 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
                     >
+                      Voir
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        strokeWidth={1.5}
+                        strokeWidth="1.5"
                         stroke="currentColor"
-                        className="w-6 h-6"
+                        className="size-6"
                       >
                         <path
                           strokeLinecap="round"
                           strokeLinejoin="round"
-                          d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                          d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                         />
                       </svg>
                     </button>
