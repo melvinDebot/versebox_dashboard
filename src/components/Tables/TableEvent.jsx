dbb746d5f175f4138e8067dac7c9925e7cdcc7bb
Melvin Debot <melvin@exceenis.com>
2024-07-26 12:28:28 +0200

    add id and loading image

--- src/components/Tables/TableEvent.jsx
+++ src/components/Tables/TableEvent.jsx
@@ -7,12 +7,21 @@ const TableEvent = ({ list }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const itemsPerPage = 10;
   const navigate = useNavigate();
+  const [imageLoaded, setImageLoaded] = useState([]);
 
   // // Filtrer les éléments en fonction du terme de recherche
   const filteredList = list.filter(
     (item) =>
       item.title && item.title.toLowerCase().includes(searchTerm.toLowerCase()),
   );
+
+  const handleImageLoad = (index) => {
+    setImageLoaded((prev) => {
+      const newImageLoaded = [...prev];
+      newImageLoaded[index] = true;
+      return newImageLoaded;
+    });
+  };
 
   // Calculer l'index de début et de fin pour la pagination
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -34,68 +43,87 @@ const TableEvent = ({ list }) => {
         />
       </div>
 
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-2 flex items-center">
-          <p className="font-medium">Event Name</p>
-        </div>
-        <div className="col-span-1 hidden items-center sm:flex">
-          <p className="font-medium">Date</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Épinglé</p>
-        </div>
-        <div className="col-span-2 flex items-center">
-          <p className="font-medium">Location</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Actions</p>
-        </div>
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white">
+          <thead>
+            <tr className="w-full border-t border-stroke py-4.5 px-4 dark:border-strokedark md:px-6 2xl:px-7.5">
+              <th className="py-2 px-4 text-left font-medium">Event Name</th>
+              <th className="py-2 px-4 text-left font-medium hidden sm:table-cell">
+                Date
+              </th>
+              <th className="py-2 px-4 text-left font-medium">Épinglé</th>
+              <th className="py-2 px-4 text-left font-medium">Location</th>
+              <th className="py-2 px-4 text-left font-medium">ID</th>
+              <th className="py-2 px-4 text-left font-medium">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentItems.map((event, key) => (
+              <tr
+                className="border-t border-stroke dark:border-strokedark"
+                key={key}
+              >
+                <td className="py-2 px-4">
+                  <div className="flex flex-col sm:flex-row sm:items-center gap-4">
+                    {!imageLoaded[key] && (
+                      <div
+                        role="status"
+                        className="flex items-center justify-center h-56 max-w-sm rounded-lg animate-pulse bg-gray-700"
+                      >
+                        <svg
+                          className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 16 20"
+                        >
+                          <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                          <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z" />
+                        </svg>
+                      </div>
+                    )}
+                    <img
+                      src={event.img}
+                      alt="Product"
+                      className={`object-cover w-28 rounded-md ${imageLoaded[key] ? "block" : "hidden"}`}
+                      onLoad={() => handleImageLoad(key)}
+                    />
+                    <p className="text-sm text-black">{event.title}</p>
+                  </div>
+                </td>
+                <td className="py-2 px-4 hidden sm:table-cell">
+                  <p className="text-sm text-black">{event.startDate}</p>
+                </td>
+                <td className="py-2 px-4">
+                  <span className="bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                    {event.pin ? "pin" : "none"}
+                  </span>
+                </td>
+                <td className="py-2 px-4">
+                  <p className="text-sm text-black">{event.location}</p>
+                </td>
+                <td className="py-2 px-4">
+                  <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+                    {event.id}
+                  </span>
+                </td>
+                <td className="py-2 px-4">
+                  <button
+                    onClick={() =>
+                      navigate(`/update-event/${key}`, { state: event })
+                    }
+                    type="button"
+                    className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:focus:ring-yellow-900"
+                  >
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
-      {currentItems.map((event, key) => (
-        <div
-          className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
-          key={key}
-        >
-          <div className="col-span-2 flex items-center">
-            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-              <div className=" rounded-md">
-                <img
-                  src={event.img}
-                  alt="Product"
-                  className="object-cover w-28"
-                />
-                <p className="text-sm text-black ">{event.title}</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-span-1 hidden items-center sm:flex">
-            <p className="text-sm text-black ">{event.startDate}</p>
-          </div>
-          <div className="col-span-1 hidden items-center sm:flex">
-            <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-              {event.pin ? "pin" : "none"}
-            </span>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black ">
-              {event.subscriberDiscountText}
-            </p>
-          </div>
-          <div className="col-span-2 flex items-center">
-            <p className="text-sm ">{event.location}</p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <button
-              onClick={() => navigate(`/update-event/${key}`, { state: event })}
-              type="button"
-              className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
-            >
-              Edit
-            </button>
-          </div>
-        </div>
-      ))}
       <div className="flex justify-center mt-4 space-x-2 flex-wrap gap-5">
         {Array.from(
           { length: Math.ceil(filteredList.length / itemsPerPage) },
--- src/components/Tables/TableProduct.jsx
+++ src/components/Tables/TableProduct.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const TableProduct = ({ list }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState("");
+  const [imageLoaded, setImageLoaded] = useState([]);
   const itemsPerPage = 10;
   const navigate = useNavigate();
 
@@ -13,6 +14,14 @@ const TableProduct = ({ list }) => {
     (item) =>
       item.title && item.title.toLowerCase().includes(searchTerm.toLowerCase()),
   );
+
+  const handleImageLoad = (index) => {
+    setImageLoaded((prev) => {
+      const newImageLoaded = [...prev];
+      newImageLoaded[index] = true;
+      return newImageLoaded;
+    });
+  };
 
   // Calculer l'index de début et de fin pour la pagination
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -24,7 +33,7 @@ const TableProduct = ({ list }) => {
   return (
     <div className="rounded-sm border border-stroke bg-white shadow-default">
       <div className="py-6 px-4 md:px-6 xl:px-7.5">
-        <h4 className="text-xl font-semibold text-black ">Top Products</h4>
+        <h4 className="text-xl font-semibold text-black">Top Products</h4>
         <input
           type="text"
           placeholder="Rechercher un product..."
@@ -34,59 +43,88 @@ const TableProduct = ({ list }) => {
         />
       </div>
 
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-        <div className="col-span-3 flex items-center">
-          <p className="font-medium">Product Name</p>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Category</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Price</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Sold</p>
-        </div>
-        <div className="col-span-1 flex items-center">
-          <p className="font-medium">Actions</p>
-        </div>
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white">
+          <thead>
+            <tr className="w-full border-t border-stroke py-4.5 px-4 dark:border-strokedark md:px-6 2xl:px-7.5">
+              <th className="py-2 px-4 text-left font-medium">Product Name</th>
+              <th className="py-2 px-4 text-left font-medium hidden sm:table-cell">
+                Category
+              </th>
+              <th className="py-2 px-4 text-left font-medium">Price</th>
+              <th className="py-2 px-4 text-left font-medium">Sold</th>
+              <th className="py-2 px-4 text-left font-medium">ID</th>
+              <th className="py-2 px-4 text-left font-medium">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentItems.map((product, key) => (
+              <tr
+                className="border-t border-stroke dark:border-strokedark"
+                key={key}
+              >
+                <td className="py-2 px-4">
+                  <div className="flex flex-col sm:flex-row sm:items-center gap-4">
+                    {!imageLoaded[key] && (
+                      <div
+                        role="status"
+                        className="flex items-center justify-center h-56 max-w-sm rounded-lg animate-pulse bg-gray-700"
+                      >
+                        <svg
+                          className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 16 20"
+                        >
+                          <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                          <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z" />
+                        </svg>
+                      </div>
+                    )}
+                    <img
+                      src={product.img}
+                      alt="Product"
+                      className={`object-cover w-28 rounded-md ${imageLoaded[key] ? "block" : "hidden"}`}
+                      onLoad={() => handleImageLoad(key)}
+                    />
+
+                    <p className="text-sm text-black">{product.title}</p>
+                  </div>
+                </td>
+                <td className="py-2 px-4 hidden sm:table-cell">
+                  <p className="text-sm text-black">{product.category}</p>
+                </td>
+                <td className="py-2 px-4">
+                  <p className="text-sm text-black">{product.price}€</p>
+                </td>
+                <td className="py-2 px-4">
+                  <p className="text-sm text-meta-3">
+                    {product.subcriberPrice}€
+                  </p>
+                </td>
+                <td className="py-2 px-4">
+                  <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+                    {product.id}
+                  </span>
+                </td>
+                <td className="py-2 px-4">
+                  <button
+                    onClick={() =>
+                      navigate(`/update-product/${key}`, { state: product })
+                    }
+                    type="button"
+                    className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:focus:ring-yellow-900"
+                  >
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
-      {currentItems.map((product, key) => (
-        <div
-          className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
-          key={key}
-        >
-          <div className="col-span-3 flex items-center">
-            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-              <div className=" rounded-md">
-                <img src={product.img} alt="Product" className="object-cover" />
-              </div>
-              <p className="text-sm text-black ">{product.title}</p>
-            </div>
-          </div>
-          <div className="col-span-2 hidden items-center sm:flex">
-            <p className="text-sm text-black ">{product.category}</p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-black ">{product.price}€</p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="text-sm text-meta-3 ">{product.subcriberPrice}€</p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <button
-              onClick={() =>
-                navigate(`/update-product/${key}`, { state: product })
-              }
-              type="button"
-              className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
-            >
-              Edit
-            </button>
-          </div>
-        </div>
-      ))}
       <div className="flex justify-center mt-4 space-x-2 flex-wrap gap-5">
         {Array.from(
           { length: Math.ceil(filteredList.length / itemsPerPage) },
